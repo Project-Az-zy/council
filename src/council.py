5afa4cf76f5edd7175a3787bae3ce4f3f0cf3827
Aurif <filmosyt@gmail.com>
2025-06-10 21:14:49 +0200

    Added coloring to session output

--- src/council.py
+++ src/council.py
@@ -1,15 +1,49 @@
 import random
+from termcolor import colored
 
 print("\033[H\033[2J", end="")
 print("\033[H\033[3J", end="")
 
+class RandColor:
+    def __init__(self, strength: int):
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        t = r+g+b
+
+        self.color = (
+            int(r/t*strength),
+            int(g/t*strength),
+            int(b/t*strength),
+        )
+
+    def variation(self, strength: int) -> tuple[int, int, int]:
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        t = r+g+b
+
+        nr = min(255, max(0, int(self.color[0] + strength*r/t*random.choice([1, -1]))))
+        ng = min(255, max(0, int(self.color[1] + strength*g/t*random.choice([1, -1]))))
+        nb = min(255, max(0, int(self.color[2] + strength*b/t*random.choice([1, -1]))))
+        nt = nr+ng+nb
+        ot = self.color[0]+self.color[1]+self.color[2]
+
+        return (
+            min(255, max(0, int(nr/nt*ot))),
+            min(255, max(0, int(ng/nt*ot))),
+            min(255, max(0, int(nb/nt*ot))),
+        )
+
+
 class Councilor:
     last_id = 0
 
-    def __init__(self, phrase: str):
+    def __init__(self, phrase: str, *, base_color: RandColor):
         Councilor.last_id += 1
         self.id = Councilor.last_id
         self.phrase = phrase
+        self.color = base_color.variation(90)
 
     @property
     def real_id(self) -> str:
@@ -26,21 +60,22 @@ class CouncilSession:
     def __init__(self, phrases: list[str]):
         phrases = [*phrases]
         random.shuffle(phrases)
-        self.councilors = [Councilor(p) for p in phrases]
+        base_color = RandColor(400)
+        self.councilors = [Councilor(p, base_color=base_color) for p in phrases]
 
     def session(self) -> None:
         councilor_names = [c.real_id for c in self.councilors]
         councilor_names.sort()
-        print(f"[ Session convened: {', '.join(councilor_names)} ]")
+        print(colored(f"[ Session convened: {', '.join(councilor_names)} ]", "white", attrs=["bold"]))
         rounds = 8
         last_speaker = None
 
         for i in range(rounds):
-            speaker = random.choice([c for c in self.councilors if last_speaker != c])
+            speaker: Councilor = random.choice([c for c in self.councilors if last_speaker != c])
             last_speaker = speaker
-            print(speaker.respond())
+            print(colored(speaker.respond(), speaker.color))
 
         for c in self.councilors:
-            print(c.respond_final())
+            print(colored(c.respond_final(), c.color))
 
 CouncilSession(["beep", "boop", "blep", "blop?"]).session()
